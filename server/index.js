d786eb99add48926eeeb8e55d5d327dfd196d780
flakvard <marni@falkvard.com>
2023-05-04 15:19:44 +0200

    client angular and common

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var socket = require('socket-io')
+var connection = require('../common/connection.js')
 
 var server = app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
@@ -10,6 +11,13 @@ var socketIO = socket(server);
 
 socketIO.on('connection', (socket)=> {
     console.log('A connection has been created with ' + socket.id)
+    socket.on(connection.change, (changes) => {
+        socketIO.sockets.emit(connection.change, changes);
+    });
+
+    socket.on(connection.create, (newData) => {
+        socketIO.socket.emit(connection.create, newData);
+    });
 });
 
 // const port = process.env.PORT || port;
